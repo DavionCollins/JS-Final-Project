1781d7099e5209f419defa04638c80ae6b3f5d04
DavionCollins <davioncollins3461@gmail.com>
2025-12-14 16:05:08 -0600

    Finished displaying the movies dynamically. Will work on the filter tomorrow.

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
 // https://www.omdbapi.com/?i=tt3896198&apikey=e56ee402&s=fast
 
-const movies = "https://www.omdbapi.com/?i=tt3896198&apikey=e56ee402&s=fast"
 
-async function main() {
-    const response = await fetch(movies)
-    const data = await response.json()
-    console.log(data)
+const movieListEl = document.querySelector(".movie__list")
+
+
+async function onSearchChange(event) {
+    const Search = event.target.value.trim()
+
+    if (!Search) {
+    movieListEl.innerHTML = ""
+    return
 }
 
-main()
+    const moviesURL = `https://www.omdbapi.com/?i=tt3896198&apikey=e56ee402&s=${Search}`
+    const movies = await fetch(moviesURL)
+    const moviesData = await movies.json()
+
+    if (!moviesData.Search) {
+    movieListEl.innerHTML = "<p>No movies found</p>"
+    return
+}
+
+  movieListEl.innerHTML = moviesData.Search.map((Search) => movieHtml(Search)).join("")
+
+
+
+
+
+
+}
+
+
+function showMovieDetails(Title) {
+    localStorage.setItem("Title", Title)
+}
+
+function movieHtml(Search) {
+return `
+        <div class="movie">
+        <img class="movie__poster" src="${Search.Poster}">
+          <h3 class="movie__title">${Search.Title}</h3>
+          <p class="movie__year">${Search.Year}</p>
+      </div>`
+}
+
