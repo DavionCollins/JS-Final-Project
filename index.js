8fbdfbc49028f98171c16fa9439d59282568fbf5
DavionCollins <davioncollins3461@gmail.com>
2025-12-15 11:12:22 -0600

    Project mostly finished. Need to make responsive and add js for mobile menu.

--- index.js
+++ index.js
@@ -2,32 +2,61 @@
 
 
 const movieListEl = document.querySelector(".movie__list")
-
+const filterEl = document.getElementById("search__result")
+let movies = []
 
 async function onSearchChange(event) {
     const Search = event.target.value.trim()
 
     if (!Search) {
     movieListEl.innerHTML = ""
+    movies = []
     return
 }
 
     const moviesURL = `https://www.omdbapi.com/?i=tt3896198&apikey=e56ee402&s=${Search}`
-    const movies = await fetch(moviesURL)
-    const moviesData = await movies.json()
+    const response = await fetch(moviesURL)
+    const moviesData = await response.json()
 
     if (!moviesData.Search) {
     movieListEl.innerHTML = "<p>No movies found</p>"
     return
 }
 
+movies = moviesData.Search
+
   movieListEl.innerHTML = moviesData.Search.map((Search) => movieHtml(Search)).join("")
 
 
 
 
+}
 
+filterEl.addEventListener("change", (event) => {
+    const value = event.target.value
 
+    if (!value || movies.length === 0 ) return;
+
+    if (value === "year__asc") {
+        movies.sort((a,b) => a.Year - b.Year)
+    }
+
+    else if (value === "year__dsc") {
+        movies.sort((a,b) => b.Year - a.Year)
+    }
+
+    else if (value === "name__asc") {
+        movies.sort ((a,b) => a.Title.localeCompare(b.Title))
+    }
+
+    else if (value === "name__dsc") {
+        movies.sort ((a,b) => b.Title.localeCompare(a.Title))
+    }
+        renderMovies(movies)
+})
+
+function renderMovies(movies) {
+    movieListEl.innerHTML = movies.map(movies => movieHtml(movies)).join("")
 }
 
 
@@ -43,4 +72,6 @@ return `
           <p class="movie__year">${Search.Year}</p>
       </div>`
 }
+
+
 
